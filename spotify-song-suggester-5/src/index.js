bbd4d2f0264316f29d991d814592af2c7b1ac0d9
DenisChuvilin <denischuv@gmail.com>
2020-05-26 09:34:04 -0700

    fixed bug, node modules inside of working folder. Redux is good to go

--- spotify-song-suggester-5/src/index.js
+++ spotify-song-suggester-5/src/index.js
@@ -16,10 +16,13 @@ const store = createStore(combineReducers, applyMiddleware(logger));
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
