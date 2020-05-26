7b565ab775ce380d5b0c7bac4bf7de3a0e75684f
jasonelliots <j.elliotschwartz@gmail.com>
2020-05-26 15:23:21 -0700

    added navbar

--- spotify-song-suggester-5/src/styledComponents.js
+++ spotify-song-suggester-5/src/styledComponents.js
@@ -1,54 +1,50 @@
 import styled from "styled-components";
 
+
 const FormContainer = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
 
   font-family: "Montserrat", sans-serif;
-  color: #1DB954;  
-  /* border: 2px solid #1DB954; 
-  border-radius: 6px; 
-  width: 35%; 
-  margin: 10% auto; 
-  background-color: #121212;  */ 
+  color: #1db954;
 
   /* * {
     border: 1px solid white;
   } */
 
   .formBox {
-    color: #1DB954; 
-  border: 3px solid #1DB954; 
-  border-radius: 6px; 
-  width: 35%; 
-  margin: 10% auto; 
-  margin-bottom: 5%; 
-  background-color: #121212; 
+    color: #1db954;
+    border: 3px solid white;
+    border-radius: 6px;
+    width: 35%;
+    margin: 10% auto;
+    margin-bottom: 5%;
+    background-color: #121212;
   }
 
   h1 {
-      text-align: center; 
+    text-align: center;
   }
 
   a {
-      text-decoration: none; 
-      color: #B3B3B3; 
+    text-decoration: none;
+    color: #b3b3b3;
   }
 
   button {
-      border: 2px solid #B3B3B3;  
-      border-radius: 5px; 
-      width: 80px; 
-      height: 40px; 
-      text-align: center; 
-      font-size: 1.1em; 
-      background-color: black; 
-      color: #B3B3B3; 
+    border: 2px solid #b3b3b3;
+    border-radius: 5px;
+    width: 90px;
+    height: 40px;
+    text-align: center;
+    font-size: 1.1em;
+    font-weight: bold;
+    background-color: black;
+    color: #b3b3b3;
 
-      :hover{
-        border: 3px solid #1DB954;
-        color: #1DB954; 
+    :hover {
+      border: 3px solid #1db954;
+      color: #1db954;
     }
-
   }
 
   form {
@@ -56,7 +52,6 @@ const FormContainer = styled.div`
     width: 100%;
     text-align: center;
     border-radius: 5px;
-
   }
 
   label {
@@ -64,26 +59,59 @@ const FormContainer = styled.div`
     align-items: flex-end;
     justify-content: space-between;
     width: 60%;
-    margin: 10% auto; 
-    margin-bottom: 7%; 
-    
+    margin: 10% auto;
+    margin-bottom: 7%;
   }
 
   input {
     height: 20px;
-    align-content: right;  
+    align-content: right;
   }
 
-
-  
   .switch {
     /* text-align: center; */
-    width: 27%; 
-    margin: 0 auto; 
-    display: flex; 
-    justify-content: space-between; 
-
+    width: 27%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
   }
 `;
 
-export { FormContainer };
+const NavBarStyles = styled.div`
+
+header {
+  background-color: #121212;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1% 5%;
+}
+
+.header-logo {
+  width: 8%;
+}
+
+img {
+    width: 100%; 
+}
+
+header .header-links {
+  width: 25%;
+  display: flex;
+  justify-content: space-around;
+}
+
+header .header-links a {
+  color: white;
+  font-size: 1.2rem;
+  font-weight: 500;
+  text-decoration: none; 
+
+  :hover {
+      color: #1db954; 
+  }
+}
+
+`;
+
+export { FormContainer, NavBarStyles };
