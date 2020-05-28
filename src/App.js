1e499373ded72a104191ef9d6dffd1ca8b50151d
jasonelliots <j.elliotschwartz@gmail.com>
2020-05-27 18:29:16 -0700

    fixed LogIn file name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 // import components
 import './App.css';
-import LogIn from './forms/Login';
+import LogIn from './forms/LogIn';
 import SignUp from './forms/SignUp';
 import NavBar from './components/NavBar';
 import UserNavBar from './components/UserNavBar';
