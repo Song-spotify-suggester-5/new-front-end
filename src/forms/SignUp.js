c8bf3a6558183c52641ca9923a564b4c6d4333ae
DenisChuvilin <denischuv@gmail.com>
2020-05-27 17:27:35 -0700

    test

---
+++ src/forms/SignUp.js
@@ -0,0 +1,46 @@
+//React 1 guy
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FormContainer } from '../styledComponents';
+
+export default function SignUp(props) {
+  const { formValues, onInputChange, errors, SignupSubmit } = props;
+
+  return (
+    <FormContainer>
+      <div className="formBox">
+        <h1> Sign Up </h1>
+        <form onSubmit={SignupSubmit}>
+          <label>
+            <span>Name: </span>
+            <input value={formValues.name} onChange={onInputChange} name="name" type="text" />
+          </label>
+          <div className="errors">{errors.name}</div>
+
+          {/* <label>
+            <span> Email: </span>
+            <input value={formValues.email} onChange={onInputChange} name="email" type="text" />
+          </label>
+          <div className="errors">{errors.email}</div> */}
+
+          <label>
+            <span> Password:</span>
+            <input value={formValues.password} onChange={onInputChange} name="password" type="text" />
+          </label>
+          <div className="errors">{errors.password}</div>
+
+          <button> Sign Up </button>
+        </form>
+      </div>
+
+      <div className="switch">
+        <p>Already have an account?</p>
+        <button>
+          {' '}
+          <Link to="/login">Log In</Link>{' '}
+        </button>
+      </div>
+    </FormContainer>
+  );
+}
